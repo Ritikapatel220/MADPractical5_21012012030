de43b81cf0398bc8b5bf5d3a7a4e7ba09f73ecba
Ritikapatel220 <111005666+Ritikapatel220@users.noreply.github.com>
2022-10-12 17:35:26 -0700

    Initial commit

---
+++ app/src/main/java/com/example/madpractical5_21012012030/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.madpractical5_21012012030
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import com.example.madpractical5_21012012003.databinding.ActivityMainBinding
+
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var togglePlayButton=true;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window,false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPlay.setOnClickListener {
+            play()
+        }
+
+        binding.btnStop.setOnClickListener {
+            stop()
+        }
+    }
+    private fun play(){
+        Intent(applicationContext,MyService::class.java).putExtra(MyService.DATA_KEY,MyService.DATA_VALUE).apply { startService(this) }
+    }
+
+
+    private  fun stop(){
+        Intent(applicationContext,MyService::class.java).apply { stopService(this) }
+    }
+
+
+}
